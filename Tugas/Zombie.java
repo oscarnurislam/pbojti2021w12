5ece40135aa235cdbc0719d8c5267e7ce08041f6
oscarnurislam <54560757+oscarnurislam@users.noreply.github.com>
2020-11-25 19:37:18 +0700

    JS012_1941720069_OscarNurIslam
    Jobsheet 12: polimorfisme

---
+++ Tugas/Zombie.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author USER
+ */
+public abstract class Zombie implements Destroyable{
+    protected int health;
+    protected int level;
+    
+    abstract public void heal();
+    
+    @Override
+    abstract public void destroyed();
+    
+
+    String getZombieInfo() {
+        return "Health = " + this.health + "\n"
+                + "Level = "+this.level + "\n";
+    }
+
+    
+}
